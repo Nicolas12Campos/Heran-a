8ba4bb1e067cbb43e5259bb6de92ab7602eceb12
Nicolas12Campos <nicolas.campos13@etec.sp.gov.br>
2025-09-08 10:57:10 -0300

    Add files via upload

---
+++ herança/src/herança/Pessoa.java
@@ -0,0 +1,16 @@
+package herança;
+
+public class Pessoa {
+		
+	private String nome;
+	private String cpf;
+	
+	public Pessoa(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	
+	public Pessoa() {
+		this("","");
+	}
+}
---
+++ herança/src/herança/menu.java
@@ -0,0 +1,15 @@
+package herança;
+
+public class menu {
+	public static void main (String[ ]args) {
+	
+	int op = 0;
+	
+	Aluno aluno = new Aluno();
+	Professor professor = new Professor();
+	Pessoa pessoa = new Pessoa();
+	
+	do{
+		
+	}while(op != 1);
+}}
